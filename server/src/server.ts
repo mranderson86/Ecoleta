2e7f9d8070396e5d0a6b41430655ea88c775a37c
Anderson <anderson@datapraxis.com.br>
2020-06-04 16:36:17 -0300

    Criado estrutura de tabelas

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,8 @@ import Routes from "./routes";
 const app = express();
 const port = 3333;
 
+app.use(express.json());
+
 app.use(Routes);
 
 app.listen(port, () => console.log("Server is running"));
