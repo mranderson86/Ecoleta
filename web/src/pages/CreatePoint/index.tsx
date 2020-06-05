e19aef246e95c94339430a2ed3a3e54eee2ef4b6
Anderson <anderson@datapraxis.com.br>
2020-06-05 11:48:00 -0300

    Listagem de Items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { Map, TileLayer, Marker } from "react-leaflet";
 
+import api from "../../services/api";
+
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    (async function handleGetItems() {
+      const response = await api.get("items");
+
+      if (response.data) {
+        setItems(response.data);
+      }
+    })();
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -88,15 +108,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oleo de Cozinha</span>
-            </li>
-
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oleo de Cozinha</span>
-            </li>
+            {items.map((item) => (
+              <li key={item.id} className="selected">
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
 
