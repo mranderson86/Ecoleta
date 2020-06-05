c1369476d0fa90952a6e10685f3d79fa12fbedd0
Anderson <anderson@datapraxis.com.br>
2020-06-05 11:07:05 -0300

    Criado Map de localização

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { Map, TileLayer, Marker } from "react-leaflet";
 
 import "./styles.css";
 import logo from "../../assets/logo.svg";
@@ -54,6 +55,15 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
+          <Map center={[-23.5593129, -46.8429662]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-23.5593129, -46.8429662]} />
+          </Map>
+
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
@@ -89,6 +99,8 @@ const CreatePoint = () => {
             </li>
           </ul>
         </fieldset>
+
+        <button type="submit">Cadastrar ponto de coleta</button>
       </form>
     </div>
   );
