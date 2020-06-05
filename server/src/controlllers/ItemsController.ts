c938f70e1fb3146051218575b0fd89fbdb550582
Anderson <anderson@datapraxis.com.br>
2020-06-05 19:38:29 -0300

    Ajuste na listagem de items

--- server/src/controlllers/ItemsController.ts
+++ server/src/controlllers/ItemsController.ts
@@ -2,15 +2,18 @@ import { Request, Response } from "express";
 import tables from "../database/connection";
 
 class ItemsController {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) { }
 
   async index(request: Request, response: Response) {
     const items = await tables("items").select("*");
+
+    console.log(items);
 
     const serializedItems = items.map((item) => {
       return {
         title: item.title,
         image_url: `http://localhost:3333/uploads/${item.image}`,
+        id: item.id
       };
     });
 
