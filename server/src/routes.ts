a4c02e2a02f38af0c2d308da136336efec3dc13f
mranderson86 <mr.anderson.brito@gmail.com>
2020-06-07 13:32:00 -0300

    Adicionado validação de cadastro

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
+
 import multer from "multer";
 
 import PointsController from "./controlllers/PointController";
@@ -17,6 +19,22 @@ Routes.get("/items", itemsController.index);
 Routes.get("/points/:id", pointsController.show);
 Routes.get("/points", pointsController.index);
 
-Routes.post("/points", upload.single("image"), pointsController.create);
+Routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      longitude: Joi.number().required(),
+      latitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().max(2),
+      items: Joi.string().required(),
+    }),
+  }),
+  pointsController.create
+);
 
 export default Routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,22 @@
 import express, { response } from "express";
 import path from "path";
 import cors from "cors";
+import { errors } from "celebrate";
 
 import Routes from "./routes";
 
 const app = express();
 const port = 3333;
 
+// Middlewares
 app.use(cors());
-
 app.use(express.json());
 
+// Rotas
 app.use(Routes);
-
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+// Validações
+app.use(errors());
 
 app.listen(port, () => console.log("Server is running"));
